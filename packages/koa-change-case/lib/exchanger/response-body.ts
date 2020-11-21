81b131a85e119d40045b9961f9fce7397a80fe4d
Ara Park <kdPark0723@gmail.com>
2020-11-21 17:24:48 +0900

    hotfix/status_error (#10)

--- packages/koa-change-case/lib/exchanger/response-body.ts
+++ packages/koa-change-case/lib/exchanger/response-body.ts
@@ -2,7 +2,9 @@ import Exchanger from "./exchanger";
 
 const responseBody: Exchanger = {
   inject: (ctx, value): void => {
-    ctx.response.body = value;
+    if (value !== undefined) {
+      ctx.response.body = value;
+    }
   },
   extract: (ctx) => ctx.response.body,
 };
